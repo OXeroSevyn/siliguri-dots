419cda70f18b60267926e54a5da6bbed10a08217
OXeroSevyn <subhamdey.one@gmail.com>
2026-02-21 14:15:55 +0530

    Initial commit for Siliguri News static site

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brandYellow: '#FFF200',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                serif: ['Playfair Display', 'serif'],
+                mono: ['Space Mono', 'monospace'],
+            }
+        }
+    }
+}
